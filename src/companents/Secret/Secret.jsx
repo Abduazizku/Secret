154f88c20676f063fffb6aa71426360b707ab378
Abduazizku <dilnozabarimova@gmail.com>
2025-09-20 21:28:14 +0500

    edit 2-input

--- src/companents/Secret/Secret.jsx
+++ src/companents/Secret/Secret.jsx
@@ -1,32 +1,34 @@
 import React, { useState } from 'react';
 import Button from '../Button/Button';
 import Input from '../Input/Input';
-import "./style.css"
 
 const DualSecret = () => {
-     const [showInput1, setShowInput1] = useState(true);
-     const [showInput2, setShowInput2] = useState(true);
+  const [showInput1, setShowInput1] = useState(true);
+  const [showInput2, setShowInput2] = useState(true);
+  const [inputValue, setInputValue] = useState('');
 
-     const toggleInput1 = () => setShowInput1(prev => !prev);
-     const toggleInput2 = () => setShowInput2(prev => !prev);
+  const toggleInput1 = () => setShowInput1(prev => !prev);
+  const toggleInput2 = () => setShowInput2(prev => !prev);
 
-     return (
-          <div className='div'>
-               <Button onClick={toggleInput1} title='Secret' />
-               {showInput1 && (
-                    <div>
-                         <Input type="text" placeholder="Input 1" />
-                    </div>
-               )}
+  return (
+    <div className='div'>
+      <Button onClick={toggleInput1} title='Secret'/>
+      {showInput1 && (
+        <div>
+          <Input type="text" pl="Input 1" />
+        </div>
+      )}
 
-               <Button onClick={toggleInput2} title='Secret'/>
-                    {showInput2 && (
-                         <div>
-                              <Input type="text" placeholder="Input 2" />
-                         </div>
-                    )}
-          </div>
-     );
+      <Button onClick={toggleInput2} style={{ marginTop: '20px' }} title='Secret'/>
+      {showInput2 && (
+        <div>
+          <Input type="text" pl="Input 2"
+          value={inputValue}
+          onChange={e => setInputValue(e.target.value)} />
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default DualSecret;
